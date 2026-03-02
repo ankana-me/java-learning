c7dfb36d14e70586327dd993bee6d9bfdf4a759e
Ankana Biswas <ankanabiswas15@gmail.com>
2026-03-02 22:51:59 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        double height = 5.3;
+        char grade = 'A';
+        String name = "Ankana";
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(name);
+    }
+}
